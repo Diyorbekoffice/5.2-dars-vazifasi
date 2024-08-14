02ca8399a27b3cfdb1f14f965f4eb351e795ac15
Diyorbekoffice <diyorbekboymirzayev0@gmail.com>
2024-08-15 03:00:19 +0500

    new

---
+++ index.js
@@ -0,0 +1,87 @@
+// // 1-masala
+// const words = ["apple", "banana", "kiwi", "cherry", "mango"];
+
+// const sortWord = arg => arg.sort((a, b) => b.length - a.length);
+// console.log(sortWord(words));
+
+// // 2-masala
+// let array = ["apple", "banana", "kiwi", "cherry", "mango"];
+
+// const capitalLetter = arg => {
+//     let res = [];
+//     arg.forEach((element) => {
+//         let arr = element[0].toUpperCase() + element.replace(element[0], ''); 
+//         res.push(arr);
+//     });
+//     return res;
+// };
+
+// console.log(capitalLetter(array));
+
+// // 3-masala
+// let str = 'h1e2ll2o w4o85r8ld';
+
+// const clearNum = arg => arg.split('').filter(char => isNaN(char) || char === ' ').join('');
+
+// console.log(clearNum(str));
+
+// // 4-masala
+// let array = ['hello', 1, true, '123'];
+
+// let onlyStr = arg => arg.filter( char => typeof char === 'string');
+
+// console.log( onlyStr (array));
+
+// // 5-masala
+// const words = ["apple", "banana", "kiwi", "cherry", "mango"];
+
+// const sortWord = arg => arg.sort((a, b) => a.length - b.length);
+// console.log(sortWord(words));
+
+// // 6-masala
+// let str = 'hello world';
+
+// const sumStr = arg => arg.split(' ').length;
+
+// console.log(sumStr(str));
+
+// // 7-masala
+// let str = 'hello world';
+
+// const clearVowel = arg => arg.replace(/[aeiouAEIOU]/g, '');
+
+// console.log(clearVowel(str));
+
+// // 8-masala
+// let str = ['banana', 'apple'];
+
+// const maxStr = arg =>  arg.reduce((char, max) => char.length < max.length ? max : char, "" );
+
+// console.log(maxStr(str));
+
+// // 9-masala
+// let str = 'hello world';
+
+// let reverseWord = arg => arg.split(' ').map(word => word.split('').reverse().join('')).join(' ');
+
+// console.log(reverseWord(str));
+
+// // 10-masala
+// let array = ['apple', 'banana', 'apple', 'orange', 'apple'];
+// let string = 'apple';
+
+// const wordStr = (arg, str) => arg.filter(char => char === str).length;
+
+// console.log(wordStr(array, string));
+
+
+
+
+
+
+
+
+
+
+
+
